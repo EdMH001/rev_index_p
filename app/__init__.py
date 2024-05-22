22e714b0dc9da426a57fc52f2c35e21f9fa08095
EdMH001 <edimh011@gmail.com>
2024-05-21 23:37:51 -0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,37 +1,25 @@
 pip install Flask
 pip install googlesearch-python
-from flask import Flask 
-from flask import (render_template,
-    request, #para recibir la información ingresada a través de los forms
-    redirect # para redireccionar la informacion y poder mostrarla
-    )
-#from googlesearch import search
-#from googlesearch.googlesearch import GoogleSearch
 
+from flask import Flask, request
+
+import time
 from googlesearch import search
 
-
 app = Flask(__name__)
-# mysql = MySQL()
 
-# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-# # app.config['MySQL_DATABASE_PORT'] = '3306'
-# app.config['MYSQL_DATABASE_USER'] = 'root'
-# app.config['MYSQL_DATABASE_PASSWORD'] = 'escalera'
-# app.config['MYSQL_DATABASE_DB'] = 'ES_ALCANCE'
-# mysql.init_app(app)
+# Ruta para procesar un dato individual
+@app.route('/procesar_dato', methods=['GET'])
+def procesar_dato():
+    dato = request.args.get('dato')
+    # Aquí llamas a tu función de procesamiento de Python
+    resultado = procesar_dato_en_python(dato)
+    return resultado
 
-
-@app.route('/api/procesar', methods=['POST'])
-def procesar_datos():
-    data = request.json['datos']  # Recibe los datos de la columna desde Google Sheets
-    # Procesa los datos y devuelve las respuestas
-    respuestas = [tu_funcion(item) for item in data]
-    return jsonify({'respuestas': respuestas})
-
-def revisar_index(item):
+# Función de procesamiento en Python
+def procesar_dato_en_python(dato):
+    # Aquí va tu lógica de procesamient
     # Checar cada url en Google y registrar los resultados
-
     query = f'site:{item}'
     try:
         search_results = list(search(query))
@@ -40,10 +28,13 @@ def revisar_index(item):
         print(f'Error al buscar {url}. Error {e}')
         results = 0
         time.sleep(1)
+    
+    resultado = results
+    return resultado
 
-    # Aquí puedes colocar la lógica para procesar cada elemento de los datos
-    # Retorna la respuesta para un elemento específico
-    return results 
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 
 
