4043d3607d569062ac1c76c1e296b44385ece373
EdMH001 <edimh011@gmail.com>
2024-05-22 15:11:49 -0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,27 @@
 from flask import Flask, request, jsonify
-from prosc_datos import procesar_dato
-
-import time
+import time  # Asegúrate de importar cualquier módulo necesario
 from googlesearch import search
 
 app = Flask(__name__)
+
+
+def procesar_dato(dato):
+    # Procesar el dato aquí (este es solo un ejemplo simple)
+    
+    # Checar cada url en Google y registrar los resultados
+    query = f'site:{dato}'
+    try:
+        search_results = list(search(query))
+        results = 1 if any(dato in result for result in search_results) else 0
+    except Exception as e:
+        #print(f'Error al buscar {url}. Error {e}')
+        results = 0
+        time.sleep(1)
+    
+    resultado = results
+    return resultado
+
+
 
 @app.route('/procesar_dato', methods=['POST'])
 def procesar_dato_route():
