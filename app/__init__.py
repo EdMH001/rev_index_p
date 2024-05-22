53b340a98b28376df1a88283eea395dd2021c2bc
EdMH001 <edimh011@gmail.com>
2024-05-22 14:50:58 -0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,45 +1,18 @@
+# Flask app
+from flask import Flask, render_template
+
+app_flask = Flask(__name__)
+
+@app_flask.route('/')
+def index():
+    return render_template('index.html')
+
+# Streamlit app
 import streamlit as st
-import urllib.parse
-import time
-from googlesearch import search
-# Definir la función de procesamiento de datos
-def procesar_dato(dato):
-    # Realizar el procesamiento necesario
-    # Por ejemplo, simplemente devolver el dato procesado
-    # Checar cada url en Google y registrar los resultados
-    query = f'site:{dato}'
-    try:
-        search_results = list(search(query))
-        results = 1 if any(dato in result for result in search_results) else 0
-    except Exception as e:
-        #print(f'Error al buscar {url}. Error {e}')
-        results = 0
-        time.sleep(1)
-    
-    resultado = results
-    
-    return resultado
 
-# Definir la ruta de la aplicación para recibir los datos
-@st.experimental_memo()
-def app():
-    # Obtener el dato de la URL
-    dato = st.experimental_get_query_params().get("dato", "")
-    
-    # Procesar el dato
-    resultado = procesar_dato(dato)
-    
-    # Devolver el resultado
-    return resultado
+def main():
+    st.title('Mi Aplicación Streamlit')
+    st.write('Esta es una aplicación de Streamlit integrada con Flask.')
 
-# Ejecutar la aplicación
-if __name__ == "__main__":
-    resultado = app()
-    st.write(resultado)
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    main()
