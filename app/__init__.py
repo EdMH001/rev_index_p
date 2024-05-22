43d9809169f39a050058848b5a30b7ab6be6df84
EdMH001 <edimh011@gmail.com>
2024-05-22 15:41:44 -0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,8 @@
-import streamlit as st
 from flask import Flask, request, jsonify
 
-# Configuración de Flask
-flask_app = Flask(__name__)
+app = Flask(__name__)
 
-@flask_app.route('/process_data', methods=['POST'])
+@app.route('/process_data', methods=['POST'])
 def process_data():
     data = request.json  # Obtener los datos enviados desde Google Sheets
     processed_data = []  # Procesa tus datos con tu función de Python
@@ -14,21 +12,5 @@ def process_data():
 
     return jsonify(processed_data)
 
-# Configuración de Streamlit
-def main():
-    st.title('Procesamiento de datos')
-
-    # Obtén los datos ingresados por el usuario
-    data = st.text_area("Ingrese los datos aquí (separados por coma)", height=200)
-
-    if st.button("Procesar"):
-        # Llama a la función en Flask para procesar los datos
-        response = flask_app.test_client().post('/process_data', json=data.split(","))
-        processed_data = response.json
-
-        # Muestra los resultados
-        st.write("Resultados del procesamiento:")
-        st.write(processed_data)
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run(debug=True)
